c7a540be4fd3adb1542128949608cc58baf71f87
dariushinojosa <101020189+dariushinojosa@users.noreply.github.com>
2024-10-27 19:43:21 +0000

    first attempts

---
+++ app/controllers/rps_controller.rb
@@ -0,0 +1,17 @@
+
+class RpsController < ApplicationController
+
+  def rock_action
+
+  end
+
+  def paper_action
+  end
+
+  def scissors_action
+  
+  end
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
 
+  get("/rock", { :controller => "rps", :action => rock_action})
+
+  get("/paper", { :controller => "rps", :action => paper_action})
+
+  get("/scissors", { :controller => "rps", :action => scissors_action})
+
 end
