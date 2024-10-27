414466774c420cbd487cfa8640bf01d1ee32b864
dariushinojosa <101020189+dariushinojosa@users.noreply.github.com>
2024-10-27 20:12:34 +0000

    restarting codespace

--- app/controllers/rps_controller.rb
+++ app/controllers/rps_controller.rb
@@ -1,16 +1,32 @@
 
 class RpsController < ApplicationController
 
+  #def rules_action
+
+
+  #end
+
+
   def rock_action
+    @computer_move = ["rock", "paper", "scissors"].sample
 
+    if @computer_move == "rock"
+      @outcome = "tied"
+    elsif @computer_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
+    render({ :template => "game_templates/play_rock"})
   end
 
-  def paper_action
-  end
+  #def paper_action
+  #end
 
-  def scissors_action
+  #def scissors_action
   
-  end
+  #end
 
 
 
