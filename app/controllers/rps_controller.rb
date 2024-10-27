010ae32faf31f7dd8cc5951c98bc7bc63e25657d
dariushinojosa <101020189+dariushinojosa@users.noreply.github.com>
2024-10-27 20:29:02 +0000

    final

--- app/controllers/rps_controller.rb
+++ app/controllers/rps_controller.rb
@@ -1,11 +1,9 @@
 
 class RpsController < ApplicationController
 
-  #def rules_action
-
-
-  #end
-
+  def rules_action
+    render({ :template => "game_templates/rules"})
+  end
 
   def rock_action
     @computer_move = ["rock", "paper", "scissors"].sample
@@ -21,13 +19,33 @@ class RpsController < ApplicationController
     render({ :template => "game_templates/play_rock"})
   end
 
-  #def paper_action
-  #end
+  def paper_action
+    @computer_move = ["rock", "paper", "scissors"].sample
 
-  #def scissors_action
-  
-  #end
+    if @computer_move == "rock"
+      @outcome = "won"
+    elsif @computer_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+    end
 
+    render({ :template => "game_templates/play_paper"})
+  end
 
+  def scissors_action
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "lost"
+    elsif @computer_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/play_scissors"})
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  #get("/", { :controller => "rps", :action => "rules_action"})
+  get("/", { :controller => "rps", :action => "rules_action"})
 
   get("/rock", { :controller => "rps", :action => "rock_action"})
 
-  #get("/paper", { :controller => "rps", :action => "paper_action"})
+  get("/paper", { :controller => "rps", :action => "paper_action"})
 
-  #get("/scissors", { :controller => "rps", :action => "scissors_action"})
+  get("/scissors", { :controller => "rps", :action => "scissors_action"})
 
 end
